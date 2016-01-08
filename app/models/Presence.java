dbe52c6bbca9bce7ffef1ad7eadad7f2955fc29e
Dunoyer <dunoyer.yoan@gmail.com>
2016-01-08 12:50:37 +0100

    Bryan : MAJ Models

--- app/models/Presence.java
+++ app/models/Presence.java
@@ -1,7 +1,18 @@
 package models;
-//TODO
+
+import javax.persistence.*;
+
 /**
  * Created by Bigval-Mac on 08/01/16.
  */
+
+@Entity
 public class Presence {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public int id;
+    public boolean emergement;
+    public String motif;
+    public String justificatif;
 }
--- app/models/Promotion.java
+++ app/models/Promotion.java
@@ -1,7 +1,16 @@
 package models;
-//TODO
+
+import javax.persistence.*;
+
 /**
  * Created by Bigval-Mac on 08/01/16.
  */
+@Entity
 public class Promotion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public int id;
+    public String anneeScolaire;
+    public String groupe;
+    public String type;
 }
