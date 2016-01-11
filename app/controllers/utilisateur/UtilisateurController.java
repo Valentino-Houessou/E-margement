99de4f912e73d9659650f3d32a461156b4e06080
Dunoyer <dunoyer.yoan@gmail.com>
2016-01-11 17:09:35 +0100

    Commit routes pour répartition des fonctionnalités à coder

---
+++ app/controllers/utilisateur/UtilisateurController.java
@@ -0,0 +1,31 @@
+package controllers.utilisateur;
+
+import play.*;
+import play.mvc.*;
+
+import views.html.*;
+
+
+public class UtilisateurController extends Controller {
+
+
+    public static Result getUtilisateur() {
+        return Results.TODO;
+    }
+
+    public static Result updateUtilisateur() {
+        return Results.TODO;
+    }
+
+    public static Result addUtilisateur() {
+        return Results.TODO;
+    }
+
+    public static Result deleteUtilisateur() {
+        return Results.TODO;
+    }
+
+    public static Result getUtilisateurByMail() {
+        return Results.TODO;
+    }
+}
---
+++ app/views/administrateur/administrateurController.java
@@ -0,0 +1,11 @@
+package views.administrateur;
+
+import play.*;
+import play.mvc.*;
+
+import views.html.*;
+
+public class administrateurController extends Controller {
+
+
+}
