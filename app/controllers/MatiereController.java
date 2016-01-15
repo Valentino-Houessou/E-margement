1bf434e64ab63be4d64d6d79c0d94ad1b73644a2
JabCorp <bryan.jabea@gmail.com>
2016-01-15 13:22:25 +0100

    Modification du Model Matiere + Controller test

---
+++ app/controllers/MatiereController.java
@@ -0,0 +1,31 @@
+package controllers;
+
+
+import models.Matiere;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.index;
+
+/**
+ * Created by Miage on 15/01/2016.
+ */
+public  class MatiereController extends Controller {
+
+    /* Ceci est une classe test */
+    /* NE PAS TOUCHER */
+
+    public Result index() {
+        System.out.println("MatiereController.index : " + Matiere.showAll());
+        System.out.println("MatiereController.index : " + Matiere.show(1));
+        Matiere matiere = new Matiere("Test One","TO","6",24);
+        matiere.save();
+        System.out.println("ok1");
+        matiere.libelleAbregee = "T1";
+        matiere.update();
+        System.out.println("ok2");
+        matiere.delete();
+        System.out.println("ok3");
+        return ok(index.render("Your new application is ready."));
+    }
+
+}
--- app/models/Matiere.java
+++ app/models/Matiere.java
@@ -4,6 +4,7 @@ import javax.persistence.*;
 import com.avaje.ebean.*;
 
 import java.util.Collection;
+import java.util.List;
 
 @Entity
 public class Matiere extends Model {
@@ -15,14 +16,37 @@ public class Matiere extends Model {
     public String semestre;
     public int nombreHeures;
 
-
+    public Matiere(String libelle, String libelleAbregee, String semestre, int nombreHeures) {
+        this.libelle = libelle;
+        this.libelleAbregee = libelleAbregee;
+        this.semestre = semestre;
+        this.nombreHeures = nombreHeures;
+    }
     public static Finder<Long,Matiere> find = new Finder<Long, Matiere>(Matiere.class);
 
-    public static Matiere findById(long id) {
-
+    public static Matiere show(long id) {
         return find.ref(id);
     }
 
+    public static List<Matiere> showAll(){
+        return find.all();
+    }
+
+    public void insert(){
+        Ebean.save(this);
+    }
+
+    public void update(String libelle, String libelleAbregee, String semestre, String nombreHeures) {
+        this.libelle = (libelle != null) ? libelle : this.libelle;
+        this.libelleAbregee = (libelleAbregee != null) ? libelleAbregee : this.libelleAbregee;
+        this.semestre = (semestre != null) ? semestre : this.semestre;
+        this.nombreHeures = nombreHeures != null ? Integer.parseInt(nombreHeures) : this.nombreHeures ;
+        Ebean.update(this);
+    }
+
+    public void delete(){
+        Ebean.delete(this);
+    }
 
 
 }
