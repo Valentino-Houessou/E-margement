b97d50f492d878f8205c32c8b1a2e5e89e9d87e4
JabCorp <kyumasakak@msn.com>
2016-05-09 16:13:30 +0200

    Préparation Android

--- app/controllers/enseignant/EnseignantController.java
+++ app/controllers/enseignant/EnseignantController.java
@@ -1,7 +1,5 @@
 package controllers.enseignant;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import controllers.*;
 import models.Cours;
 import models.Enseignant;
 import models.Presence;
@@ -15,6 +13,7 @@ import views.html.enseignant.list_etudiants;
 
 import java.sql.Timestamp;
 import java.time.Instant;
+import java.util.HashMap;
 import java.util.List;
 
 
@@ -36,8 +35,33 @@ public class EnseignantController extends Controller{
         return ok(list_cours.render(Cours.findByEnseignant(teacher.id,dateform.get().date)));
     }
 
+    public Result androidListCours(){
+        Enseignant user = null;
+        List<Cours> coursList = null;
+        HashMap<String,String> map = new HashMap<>();
+        Form<DateForm> dateform = Form.form(DateForm.class).bindFromRequest();
+        try{
+            user = Enseignant.findByUser(Long.parseLong(dateform.get().anId));
+        }catch (NullPointerException e){
+            map.put("error","L'id de l'enseignant n'a pu être récupéré !");
+            return badRequest(Json.toJson(map));
+        }
+        try{
+            coursList = Cours.findByEnseignant(user.id,dateform.get().date);
+            if(coursList.size() == 0){
+                map.put("error","Pas de cours à cette date !");
+                return badRequest(Json.toJson(map));
+            }
+        }catch (NullPointerException e){
+            map.put("error","Pas de cours à cette date !");
+            return badRequest(Json.toJson(map));
+        }
+        return ok(Json.toJson(coursList));
+    }
+
     public static class DateForm{
         public String date;
+        public String anId;
 
         public String validate(){
             return null;
@@ -58,12 +82,67 @@ public class EnseignantController extends Controller{
         return ok(list_etudiants.render(Presence.findbyCours(Long.parseLong(coursform.get().cours))));
     }
 
+    public Result androidListPresence() {
+        Cours cours = null;
+        List<Presence> presenceList = null;
+        HashMap<String,String> map = new HashMap<>();
+        Form<CoursForm> coursform = Form.form(CoursForm.class).bindFromRequest();
+        if(coursform.get().cours.equals("-1")){
+            map.put("error","Aucun cours selectionné !");
+            return badRequest(Json.toJson(map));
+        }
+        try {
+            cours = Cours.findbyId(Long.parseLong(coursform.get().cours));
+            if(cours.signatureEnseignant){
+                map.put("error","La feuille de présence de ce cours a été signé.");
+                return badRequest(Json.toJson(map));
+            }
+            if(cours.heureDebut.after(Timestamp.from(Instant.now()))){
+                map.put("error","Le cours n'a pas commencé.");
+                return badRequest(Json.toJson(map));
+            }
+            if(Timestamp.from(cours.heureFin.toInstant().plusSeconds(900)).before(Timestamp.from(Instant.now()))){
+                map.put("error","Le cours a eu lieu.");
+                return badRequest(Json.toJson(map));
+            }
+        }catch (NullPointerException e){
+            map.put("error","Nous n'arrivons pas à récupérer le cours !");
+            return badRequest(Json.toJson(map));
+        }
+        try{
+            presenceList = Presence.findbyCours(cours.id);
+            if(presenceList.size() == 0){
+                map.put("error","La liste de présence n'a pas encore été générée !");
+                return badRequest(Json.toJson(map));
+            }
+        }catch (NullPointerException e){
+            map.put("error","Nous n'arrivons pas à récupérer la liste pour ce cours !");
+            return badRequest(Json.toJson(map));
+        }
+        return ok(Json.toJson(presenceList));
+    }
+
     public Result signature(){
         Form<CoursForm> coursform = Form.form(CoursForm.class).bindFromRequest();
         Cours cours = Cours.findbyId(Long.parseLong(coursform.get().cours));
         cours.signatureEnseignant = true;
         cours.update();
         return ok("<div class='form-group signature'><span>La feuille de présence de ce cours a été signé.</span></div>");
+    }
+
+    public Result androidSignature(){
+        HashMap<String,String> map = new HashMap<>();
+        Cours cours = null;
+        try{
+            Form<CoursForm> coursform = Form.form(CoursForm.class).bindFromRequest();
+            cours = Cours.findbyId(Long.parseLong(coursform.get().cours));
+            cours.signatureEnseignant = true;
+            cours.update();
+            return ok(Json.toJson("La feuille de présence du cours de " + cours.saMatiere.libelle + " du " + cours.heureDebut + " vient d'être signé"));
+        }catch (Exception e){
+            map.put("error","Une erreur s'est produite");
+            return badRequest(Json.toJson(map));
+        }
     }
 
     public static class CoursForm {
@@ -75,11 +154,18 @@ public class EnseignantController extends Controller{
     }
 
     public Result majPresence(){
-        Form<PresenceForm> presenceform = Form.form(PresenceForm.class).bindFromRequest();
-        Presence newPresence = Presence.findbyEtudiant(Long.parseLong(presenceform.get().idEtu),Long.parseLong(presenceform.get().cours));
-        newPresence.emergement = Boolean.parseBoolean(presenceform.get().presence);
-        newPresence.update();
-        return ok();
+        HashMap<String,String> map = new HashMap<>();
+        try {
+            Form<PresenceForm> presenceform = Form.form(PresenceForm.class).bindFromRequest();
+            Presence newPresence = Presence.findbyEtudiant(Long.parseLong(presenceform.get().idEtu), Long.parseLong(presenceform.get().cours));
+            newPresence.emergement = Boolean.parseBoolean(presenceform.get().presence);
+            newPresence.update();
+        } catch (Exception e){
+            e.printStackTrace();
+            map.put("error","Une erreur c'est produite");
+            return badRequest(Json.toJson(map));
+        }
+        return ok(Json.toJson("Présence mise à jour"));
     }
 
     public static class PresenceForm{
